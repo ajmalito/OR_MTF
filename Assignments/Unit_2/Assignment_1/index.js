865fba4a1885d00272972ab4867aa7831fdbb3e8
Ajmal Jackson-Brown <asj12@hampshire.edu>
2018-04-23 13:07:25 -0700

    assignment placeholder updates

---
+++ Assignments/Unit_2/Assignment_1/index.js
@@ -0,0 +1,27 @@
+// Write Javascript to perform the following tasks. Please submit links to your assignment on repl.it. Here is the Javascript console we used in class: repl.it
+
+// 1. Perform three basic math operations
+
+
+
+// 2. Create a variable called "myNum" and assign it a value of 3
+
+// 3. Output/print/return the value of the "myNum" variable
+
+// 4. Assign "mynum" a value of 7
+
+// 5. Output/print/return the value of "myNum"
+
+// 6. Increase the value of "myNum" by 3
+
+// 7. Output/print/return the value of "myNum"
+
+// 8. Create another variable called "myOtherNum" with value 5
+
+// 9. Compare "myNum" and "myOtherNum". Are they equal? Your code should return 'true' or 'false'
+
+// 10. Increase the value of "myOtherNum" by 5
+
+// 11. Compare "myNum" and "myOtherNum". Are they equal? Your code should return 'true' or 'false'
+
+// 12. Create a third variable called "myString" with value "10" (note: "2" should be a string, NOT a number)
---
+++ Assignments/Unit_2/Assignment_2/index.js
@@ -0,0 +1,5 @@
+// Write Javascript to perform the following tasks. Please submit links to your assignment on repl.it. Here is the Javascript console we used in class: repl.it
+
+// 1. Change the background color of The first page by selecting the appropriate ID.
+
+// 2.
