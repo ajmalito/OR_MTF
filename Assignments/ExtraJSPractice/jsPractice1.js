4b27f5755559298f93ba0cc1c12c876bd145c8f7
Ajmal Jackson-Brown <asj12@hampshire.edu>
2018-05-23 10:51:53 -0700

    added in extra JS problem set

---
+++ Assignments/ExtraJSPractice/jsPractice1.js
@@ -0,0 +1,61 @@
+/*
+
+1. Declare a new variable called myNum and give it the value of 3.
+
+2. Declare a new variable called myOtherNum and give it the value of myNum plus 7. Use a Math Assignment Operator to do this.
+
+3. Create a variable called favoriteTopping and give it the value of your favorite topping.
+
+4. print this message out to the console: "my favorite pizza topping is:" whatever your favorite topping is.
+
+5. Print this message out to the console using a Template Literal (AKA Template String): "my favorite pizza topping is:" whatever your favorite topping is.
+
+6. Create a variable called tired and give it the value of false;
+
+7. Create an if-else statement that checks to see if the value of tired equals true. If it does, print "Taking a nap now". Otherwise, print "Keeping on keeping on" to the console.
+
+8. Use comments to explain what the function above does.
+
+9. Create a variable called firstName and give it the value of your first name.
+
+10. Create a variable called lastName and give it the value of your last name.
+
+11. Write an if-else statement that checks to see if your first name is longer than your last name and middle name. If it is longer than both of them, print the message "Your first name is longer than your middle name and last name!" to the console. If it is not longer than both of them, print the message "Your first name is not longer than both your middle and last name!" to the console.
+
+12. Create a variable called isNightTime and set its value to false.
+
+13. Create a variable lightsOn and set its to false.
+
+14. Create a ternary operator that checks if nightTime returns true if it is changes the value of lightsOn to true. Otherwise, se the value of lightsOn to false.
+
+15. Create a function called add that takes in two paramenters. One called numOne and one called numTwo.
+
+16. In the block of the function use the keyword return to return the sum of numberOne and numberTwo.
+
+17. Call the function add and pass in the arguments 10 and 14.
+
+18. Create a variable called currentNum.
+
+19. create a for loop that starts at i, goes as long as i is less than 10, and increments by one.
+
+20. Each time the loop executes change the value of currentNum to be equal to i, and print the value of currentNum to the console.
+
+21. Nest another for loop inside of the current block. It should start at 0, go until the current nested loop is equal to 9, and increment by 1.
+
+22. Inside of the second loop's block, make it so that each time the loop executes change the value of currentNum to be equal to j and print the value of currentNum to the console.
+
+
+23. Create a function called transformation that takes in 
+an array of strings, loops through the characters 
+of each string and shifts them forward by one in the alphabet.
+
+24. Bonus: create a conditional statement that makes vowels 
+in the final array capitalized if they are vowels.
+
+25. *Bonus +++: create a conditional statement that 
+makes vowels (AEIOU and 50% of the time y) capitalized.
+
+
+Example: ["a", "b", "c"] would return ["b", "c", "d"]
+*/
+
