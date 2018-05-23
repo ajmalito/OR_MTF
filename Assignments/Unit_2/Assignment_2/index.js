1968ea764047dc37bf1ac5de71fce4ac5f847420
Ajmal Jackson-Brown <asj12@hampshire.edu>
2018-05-23 14:36:08 -0700

    Cleaned up Unit 2 Assignment #2

--- Assignments/Unit_2/Assignment_2/index.js
+++ Assignments/Unit_2/Assignment_2/index.js
@@ -1,5 +1,18 @@
-// Write Javascript to perform the following tasks. Please submit links to your assignment on repl.it. Here is the Javascript console we used in class: repl.it
+/*
+Write Javascript to perform the following tasks. 
+Please push your changes back to your personal OR_MTF_Submissions directory. 
+*/
+
+// USE JavaSCript to complege these tasks:
 
 // 1. Change the background color of The first page by selecting the appropriate ID.
 
-// 2.
+// 2. Change the inner text of the h1 to say anything that you would like.
+
+// 3. Create a variable that is equal to the DOM object that has the id random-btn.
+
+// 4. Create a variable that is equal to the DOM object that has the id pageTitle. 
+
+// 5. Create a fucntion called randomSize that changes the font size of pageTitle to be between 1px and 100px.
+
+// 6. Create an event listener that runs the randomSize function on click.
